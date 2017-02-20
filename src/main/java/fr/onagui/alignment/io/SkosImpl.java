3db5f114c70edac5a3c0651c5c7064aae77a1ce8
clarvie <236tech@gmail.com>
2017-02-20 17:39:18 +0100

    Sauvegarde en skos

--- src/main/java/fr/onagui/alignment/io/SkosImpl.java
+++ src/main/java/fr/onagui/alignment/io/SkosImpl.java
@@ -1,16 +1,29 @@
 package fr.onagui.alignment.io;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-import com.hp.hpl.jena.rdf.model.Model;
-import com.hp.hpl.jena.rdf.model.ModelFactory;
-import com.hp.hpl.jena.rdf.model.Property;
-import com.hp.hpl.jena.rdf.model.RDFWriter;
-import com.hp.hpl.jena.rdf.model.Resource;
+import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.Model;
+import org.eclipse.rdf4j.model.Statement;
+import org.eclipse.rdf4j.model.ValueFactory;
+import org.eclipse.rdf4j.model.impl.LinkedHashModel;
+import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
+import org.eclipse.rdf4j.model.util.Models;
+import org.eclipse.rdf4j.model.vocabulary.RDF;
+import org.eclipse.rdf4j.model.vocabulary.SKOS;
+import org.eclipse.rdf4j.rio.RDFFormat;
+import org.eclipse.rdf4j.rio.RDFHandlerException;
+import org.eclipse.rdf4j.rio.RDFWriter;
+import org.eclipse.rdf4j.rio.Rio;
+
+
+
+
 
 import fr.onagui.alignment.Alignment;
 import fr.onagui.alignment.Mapping;
@@ -42,47 +55,76 @@ public class SkosImpl implements IOAlignment {
 	public <ONTORES1, ONTORES2> void saveAlignment(
 			Alignment<ONTORES1, ONTORES2> alignment, String pathToSave,
 			VALIDITY validityWanted) throws IOException {
-
+		ValueFactory factory = SimpleValueFactory.getInstance();
+		Model model = new LinkedHashModel();
+		
+		/*
 		Model model = ModelFactory.createDefaultModel();
 		model.setNsPrefix("skos", "http://www.w3.org/2004/02/skos/core#");
-		model.setNsPrefix("", "http://www.w3.org/2004/02/skos/core#");
+		model.setNsPrefix("", "http://www.w3.org/2004/02/skos/core#");*/
+		
 		
 		final OntoContainer<ONTORES1> onto1 = alignment.getOnto1();
 		final OntoContainer<ONTORES2> onto2 = alignment.getOnto2();
 		
-		// Build the mapping nodes
-		final Property exactMatch = model.createProperty("http://www.w3.org/2004/02/skos/core#exactMatch");
+		Statement typeStatement =null;
+		
+		/*final Property exactMatch = model.createProperty("http://www.w3.org/2004/02/skos/core#exactMatch");
 		final Property broadMatch = model.createProperty("http://www.w3.org/2004/02/skos/core#broadMatch");
 		final Property narrowMatch = model.createProperty("http://www.w3.org/2004/02/skos/core#narrowMatch");
-		final Property closeMatch = model.createProperty("http://www.w3.org/2004/02/skos/core#closeMatch");
+		final Property closeMatch = model.createProperty("http://www.w3.org/2004/02/skos/core#closeMatch");*/
+		
 		for (Mapping<ONTORES1, ONTORES2> mapping : alignment.getMapping()) {
 			if(validityWanted != null && !mapping.getValidity().equals(validityWanted))
 				continue;
 			
-			Property propertyToUsed = null;
+			IRI propertyToUsed = null;
+			
+			
 			if(mapping.getType() == MAPPING_TYPE.EQUIV)
-				propertyToUsed = exactMatch;
+				propertyToUsed = SKOS.EXACT_MATCH;
+				
 			else if(mapping.getType() == MAPPING_TYPE.SUBSUMES)
-				propertyToUsed = broadMatch;
+				propertyToUsed = SKOS.BROAD_MATCH;
+				
 			else if(mapping.getType() == MAPPING_TYPE.SUBSUMEDBY)
-				propertyToUsed = narrowMatch;
+				propertyToUsed = SKOS.NARROW_MATCH;
+				
 			else if(mapping.getType() == MAPPING_TYPE.OVERLAP)
-				propertyToUsed = closeMatch;
+				propertyToUsed = SKOS.CLOSE_MATCH;
+			    
 			else
 				continue;
+			IRI res1= factory.createIRI(onto1.getURI(mapping.getFirstConcept()).toString());
+			IRI res2= factory.createIRI(onto2.getURI(mapping.getSecondConcept()).toString());
+			typeStatement=factory.createStatement(res1, propertyToUsed, res2);
+			model.add(typeStatement);
 			
-			Resource res1 = model.createResource(onto1.getURI(
+			/*Resource res1 = model.createResource(onto1.getURI(
 					mapping.getFirstConcept()).toString());
 			Resource res2 = model.createResource(onto2.getURI(
 					mapping.getSecondConcept()).toString());
-			res1.addProperty(propertyToUsed, res2);
+			res1.addProperty(propertyToUsed, res2);*/
 		}
 		
-		OutputStream stream = Files.newOutputStream(Paths.get(pathToSave));
+	  /*OutputStream stream = Files.newOutputStream(Paths.get(pathToSave));
 		stream.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n".getBytes(ONAGUI_CHARSET));
 		RDFWriter w = model.getWriter("RDF/XML-ABBREV");
 		w.setProperty("showXmlDeclaration", "false");
-		w.write(model, stream, "http://www.w3.org/2004/02/skos/core#");		
+		w.write(model, stream, "http://www.w3.org/2004/02/skos/core#");*/
+		FileOutputStream stream = new FileOutputStream(pathToSave);
+		RDFWriter writer = Rio.createWriter(RDFFormat.RDFXML,stream);
+		try {
+			  writer.startRDF();
+			  for (Statement st: model) {
+			    writer.handleStatement(st);
+			  }
+			  writer.endRDF();
+			}
+			catch (RDFHandlerException e) {
+			 // oh no, do something!
+			}
+		
 	}
 }
 
