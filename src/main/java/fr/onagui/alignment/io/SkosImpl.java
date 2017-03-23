cdbd03eb4ec4726bafaaa9a944cc64c2d1e7d8f0
clarvie <236tech@gmail.com>
2017-03-23 09:09:46 +0100

    issue22 implémentation du chargement d'un alignement en SKOS

--- src/main/java/fr/onagui/alignment/io/SkosImpl.java
+++ src/main/java/fr/onagui/alignment/io/SkosImpl.java
@@ -1,21 +1,47 @@
 package fr.onagui.alignment.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributeView;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+import java.text.SimpleDateFormat;
+import java.util.Map;
+import java.util.TreeMap;
 
-import com.hp.hpl.jena.rdf.model.Model;
-import com.hp.hpl.jena.rdf.model.ModelFactory;
-import com.hp.hpl.jena.rdf.model.Property;
-import com.hp.hpl.jena.rdf.model.RDFWriter;
-import com.hp.hpl.jena.rdf.model.Resource;
+import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.Model;
+import org.eclipse.rdf4j.model.Resource;
+import org.eclipse.rdf4j.model.Statement;
+import org.eclipse.rdf4j.model.Value;
+import org.eclipse.rdf4j.model.ValueFactory;
+import org.eclipse.rdf4j.model.impl.LinkedHashModel;
+import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
+import org.eclipse.rdf4j.model.vocabulary.RDF;
+import org.eclipse.rdf4j.model.vocabulary.RDFS;
+import org.eclipse.rdf4j.model.vocabulary.SKOS;
+import org.eclipse.rdf4j.rio.RDFFormat;
+import org.eclipse.rdf4j.rio.RDFParseException;
+import org.eclipse.rdf4j.rio.Rio;
+import org.eclipse.rdf4j.rio.UnsupportedRDFormatException;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.ISODateTimeFormat;
 
 import fr.onagui.alignment.Alignment;
 import fr.onagui.alignment.Mapping;
 import fr.onagui.alignment.Mapping.MAPPING_TYPE;
 import fr.onagui.alignment.Mapping.VALIDITY;
+import fr.onagui.alignment.NoMappingPossible;
 import fr.onagui.alignment.OntoContainer;
 
 public class SkosImpl implements IOAlignment {
@@ -23,7 +49,15 @@ public class SkosImpl implements IOAlignment {
 	// Liste des namespaces
 	public static final String ALIGN_NS = "http://knowledgeweb.semanticweb.org/heterogeneity/alignment#";
 	public static final String DCTERMS_NS = "http://purl.org/dc/terms/";
-	
+	public static final String METHOD = "manual";
+
+	// Type et propriétés perso
+	private IOEventManager ioEventManager = null;
+
+
+	public SkosImpl(IOEventManager ioe) {
+		this.ioEventManager = ioe;
+	}
 	/* (non-Javadoc)
 	 * @see fr.onagui.alignment.io.IOAlignment#loadAlignment(fr.onagui.alignment.OntoContainer, fr.onagui.alignment.OntoContainer, java.io.File)
 	 */
@@ -35,7 +69,139 @@ public class SkosImpl implements IOAlignment {
 		if (file == null || !file.exists() || !file.isFile())
 			throw new IllegalArgumentException(
 					"File parameter is not a valid existing file.");
-		throw new UnsupportedOperationException("SKOS load is not yet supported");
+
+		Model model = new LinkedHashModel();
+		Path p = Paths.get(file.getAbsolutePath());
+		DateTime date=null;
+		BasicFileAttributes view;
+		try {
+			view = Files.getFileAttributeView(p, BasicFileAttributeView.class)
+					.readAttributes();
+			FileTime fileTime=view.creationTime();
+			date=new DateTime(fileTime.toMillis());
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		try {
+			model=readModel(file,model);
+			System.out.println("taille model: "+model.size());
+		} catch (Exception e) {
+			// Impossible, j'ai testé l'existence au dessus...
+			e.printStackTrace();
+			throw new IllegalArgumentException("Unable to read file. Right problem?");
+		}
+
+		Alignment<ONTORES1, ONTORES2> alignment = new Alignment<ONTORES1, ONTORES2>(
+				onto1, onto2);
+
+		for (Statement unTriplet: model) {
+			System.out.println("unTriplet : "+unTriplet);	
+
+			if(
+					unTriplet.getPredicate().equals(SKOS.EXACT_MATCH)
+					||
+					unTriplet.getPredicate().equals(SKOS.CLOSE_MATCH)
+					||
+					unTriplet.getPredicate().equals(SKOS.BROAD_MATCH)
+					||
+					unTriplet.getPredicate().equals(SKOS.NARROW_MATCH)
+					||
+					unTriplet.getPredicate().equals(SKOS.RELATED_MATCH)
+					) {
+
+				// Manage method
+				String method = METHOD;
+				// Manage validity
+				VALIDITY valid =VALIDITY.TO_CONFIRM;
+				// Manage creation date				
+				//DateTime date = new DateTime();
+				double score =1.0;
+
+				MAPPING_TYPE type = null;
+				if(unTriplet.getPredicate().equals(SKOS.EXACT_MATCH)) {
+					type = MAPPING_TYPE.EQUIV;
+				}
+				else if(unTriplet.getPredicate().equals(SKOS.CLOSE_MATCH)) {
+					type = MAPPING_TYPE.OVERLAP;
+				}
+				else if(unTriplet.getPredicate().equals(SKOS.BROAD_MATCH)) {
+					type = MAPPING_TYPE.SUBSUMES;
+				}
+				else if(unTriplet.getPredicate().equals(SKOS.NARROW_MATCH)) {
+					type = MAPPING_TYPE.SUBSUMEDBY;
+				}
+				else if(unTriplet.getPredicate().equals(SKOS.RELATED_MATCH)) {
+					type = MAPPING_TYPE.RELATED;
+				} else {
+					String errMsg = "Oops, should not happen, found an unexpected predicate "+unTriplet.getPredicate();
+					ioEventManager.inputEvent(errMsg);
+					continue;
+				}
+
+
+				Mapping<ONTORES1, ONTORES2> map;
+				try {
+					String uri1 = unTriplet.getSubject().stringValue();
+					String uri2 = unTriplet.getObject().stringValue();
+					// Prepare errMSg, maybe....
+					String errMsg = "Loading alignment between " + uri1
+							+ " and " + uri2
+							+ " failed because this concepts does not exist: ";
+					ONTORES1 cpt1 = onto1.getConceptFromURI(new URI(uri1));
+					if (cpt1 == null) {
+						ioEventManager.inputEvent(errMsg + uri1);
+						continue;
+					}
+					ONTORES2 cpt2 = onto2.getConceptFromURI(new URI(uri2));
+					if (cpt2 == null) {
+						ioEventManager.inputEvent(errMsg + uri2);
+						continue;
+					}			
+
+					map = new Mapping<ONTORES1, ONTORES2>(
+							cpt1, cpt2, score, type, method, valid, date);
+
+					alignment.addMap(map);
+				} catch (URISyntaxException e) {
+					ioEventManager.inputEvent("One of the value is not a valid uri");
+					e.printStackTrace();
+				}
+
+
+			} else {
+				System.out.println("Cannot understand predicate "+unTriplet.getPredicate());
+			}
+
+
+		}
+
+		return alignment;
+	}
+
+	public Model readModel(File file,Model model ){
+		try {
+			RDFFormat format = Rio.getParserFormatForFileName(file.getAbsolutePath()).orElse(RDFFormat.RDFXML);
+			InputStream inputStream= new FileInputStream(file);
+			try {
+				model= Rio.parse(inputStream, file.getAbsolutePath(),format);
+			} catch (RDFParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (UnsupportedRDFormatException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return model;
 	}
 
 	@Override
@@ -43,46 +209,47 @@ public class SkosImpl implements IOAlignment {
 			Alignment<ONTORES1, ONTORES2> alignment, String pathToSave,
 			VALIDITY validityWanted) throws IOException {
 
-		Model model = ModelFactory.createDefaultModel();
-		model.setNsPrefix("skos", "http://www.w3.org/2004/02/skos/core#");
-		model.setNsPrefix("", "http://www.w3.org/2004/02/skos/core#");
-		
+		ValueFactory factory = SimpleValueFactory.getInstance();
+		Model model = new LinkedHashModel();
+		model.setNamespace("skos", "http://www.w3.org/2004/02/skos/core#");
+		model.setNamespace("", "http://www.w3.org/2004/02/skos/core#");
+
+
 		final OntoContainer<ONTORES1> onto1 = alignment.getOnto1();
 		final OntoContainer<ONTORES2> onto2 = alignment.getOnto2();
-		
-		// Build the mapping nodes
-		final Property exactMatch = model.createProperty("http://www.w3.org/2004/02/skos/core#exactMatch");
-		final Property broadMatch = model.createProperty("http://www.w3.org/2004/02/skos/core#broadMatch");
-		final Property narrowMatch = model.createProperty("http://www.w3.org/2004/02/skos/core#narrowMatch");
-		final Property closeMatch = model.createProperty("http://www.w3.org/2004/02/skos/core#closeMatch");
+
+		Statement typeStatement =null;
+
+
 		for (Mapping<ONTORES1, ONTORES2> mapping : alignment.getMapping()) {
 			if(validityWanted != null && !mapping.getValidity().equals(validityWanted))
 				continue;
-			
-			Property propertyToUsed = null;
+
+			IRI propertyToUsed = null;
+
+
 			if(mapping.getType() == MAPPING_TYPE.EQUIV)
-				propertyToUsed = exactMatch;
+				propertyToUsed = SKOS.EXACT_MATCH;
+
 			else if(mapping.getType() == MAPPING_TYPE.SUBSUMES)
-				propertyToUsed = broadMatch;
+				propertyToUsed = SKOS.BROAD_MATCH;
+
 			else if(mapping.getType() == MAPPING_TYPE.SUBSUMEDBY)
-				propertyToUsed = narrowMatch;
+				propertyToUsed = SKOS.NARROW_MATCH;
+
 			else if(mapping.getType() == MAPPING_TYPE.OVERLAP)
-				propertyToUsed = closeMatch;
+				propertyToUsed = SKOS.CLOSE_MATCH;
+
 			else
 				continue;
-			
-			Resource res1 = model.createResource(onto1.getURI(
-					mapping.getFirstConcept()).toString());
-			Resource res2 = model.createResource(onto2.getURI(
-					mapping.getSecondConcept()).toString());
-			res1.addProperty(propertyToUsed, res2);
+			IRI res1= factory.createIRI(onto1.getURI(mapping.getFirstConcept()).toString());
+			IRI res2= factory.createIRI(onto2.getURI(mapping.getSecondConcept()).toString());
+			typeStatement=factory.createStatement(res1, propertyToUsed, res2);
+			model.add(typeStatement);
 		}
-		
-		OutputStream stream = Files.newOutputStream(Paths.get(pathToSave));
-		stream.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n".getBytes(ONAGUI_CHARSET));
-		RDFWriter w = model.getWriter("RDF/XML-ABBREV");
-		w.setProperty("showXmlDeclaration", "false");
-		w.write(model, stream, "http://www.w3.org/2004/02/skos/core#");		
+
+		FileOutputStream stream = new FileOutputStream(pathToSave);
+		Rio.write(model, stream, RDFFormat.RDFXML);	
 	}
 }
 
