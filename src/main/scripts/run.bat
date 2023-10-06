fc78b32e54cf9503a2a8dad0bfab81b64eca8da6
Thomas Francart <thomas.francart@sparna.fr>
2023-10-06 09:48:47 +0200

    Fix #89

--- src/main/scripts/run.bat
+++ src/main/scripts/run.bat
@@ -4,11 +4,12 @@
 
 :: java command line, modern Oracle allows to use juste "java"
 set JAVA=java
-
+
+
 
 :: Change this line and make the right amount of memory for Java if you have a "Java Heap Space" error (big ontologies)
 
-set JAVA=%JAVA% -Xmx1024m
+set JAVA=%JAVA% -Xmx1024m -Djava.util.Arrays.useLegacyMergeSort=true
 
 :: This is a hack to start the jar whatever its name
 for %%i in (onagui*.jar) do start "OnAGUI" /b %JAVA% -jar %%i
